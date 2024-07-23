b563509d9175acca9d9dedc523f5bb5f9a106e31
Brian <brian_sjoberg@yahoo.com>
2024-07-23 08:21:17 -0400

    Updated to junit 5, cucumber bom, java 17. Moved some stuff around. Update jacoco as well.

--- src/test/java/RunCukeTests.java
+++
@@ -1,9 +0,0 @@
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-import org.junit.runner.RunWith;
-
-@RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty", "junit:target/cucumber-reports/output.xml"},
-                features = "src/test/resources")
-public class RunCukeTests {
-}
