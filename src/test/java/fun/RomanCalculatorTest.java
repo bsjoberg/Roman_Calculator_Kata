1a7257187896b737559f9d1477683712487e160c
Brian <brian_sjoberg@yahoo.com>
2024-07-31 09:34:03 -0400

    Forgot more.

--- src/test/java/fun/RomanCalculatorTest.java
+++ src/test/java/fun/RomanCalculatorTest.java
@@ -8,29 +8,29 @@ public class RomanCalculatorTest {
 
     @Test
     public void should_convert_Roman_to_int() {
-        assertEquals(1, RomanCalculator.toInt("I"));
-        assertEquals(2, RomanCalculator.toInt("II"));
-        assertEquals(3, RomanCalculator.toInt("III"));
-        assertEquals(6, RomanCalculator.toInt("VI"));
-        assertEquals(10, RomanCalculator.toInt("X"));
+        assertEquals(1, RomanCalculator.fromRomanToInt("I"));
+        assertEquals(2, RomanCalculator.fromRomanToInt("II"));
+        assertEquals(3, RomanCalculator.fromRomanToInt("III"));
+        assertEquals(6, RomanCalculator.fromRomanToInt("VI"));
+        assertEquals(10, RomanCalculator.fromRomanToInt("X"));
     }
 
     @Test
     public void should_convert_complicated_Roman_to_int() {
-        assertEquals(4, RomanCalculator.toInt("IV"));
-        assertEquals(9, RomanCalculator.toInt("IX"));
+        assertEquals(4, RomanCalculator.fromRomanToInt("IV"));
+        assertEquals(9, RomanCalculator.fromRomanToInt("IX"));
     }
 
     @Test
     public void should_convert_int_to_Roman() {
-        assertEquals("II", RomanCalculator.toRoman(2));
-        assertEquals("III", RomanCalculator.toRoman(3));
-        assertEquals("VIII", RomanCalculator.toRoman(8));
+        assertEquals("II", RomanCalculator.fromIntToRoman(2));
+        assertEquals("III", RomanCalculator.fromIntToRoman(3));
+        assertEquals("VIII", RomanCalculator.fromIntToRoman(8));
     }
 
     @Test
     public void should_convert_int_using_subtractive_Roman() {
-        assertEquals("IV", RomanCalculator.toRoman(4));
-        assertEquals("IX", RomanCalculator.toRoman(9));
+        assertEquals("IV", RomanCalculator.fromIntToRoman(4));
+        assertEquals("IX", RomanCalculator.fromIntToRoman(9));
     }
 }
