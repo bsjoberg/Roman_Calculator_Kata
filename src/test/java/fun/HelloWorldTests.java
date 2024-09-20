ab4c20c3779866fef9d3a8eb829bfc7c2105a710
Brian <brian_sjoberg@yahoo.com>
2024-09-20 07:28:20 -0400

    Change file names

--- src/test/java/fun/HelloWorldTests.java
+++ src/test/java/fun/HelloWorldTests.java
@@ -4,7 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-public class HelloTests {
+public class HelloWorldTests {
 
     @Test
     public void should_say_hi() {
